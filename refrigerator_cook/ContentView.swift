e0bff51cd138410b1d4ef0ab79a39fae727079c8
realdongchimi <qjqwjdtm@naver.com>
2025-11-18 15:47:32 +0900

    Initial Commit

---
+++ refrigerator_cook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  refrigerator_cook
+//
+//  Created by dsu_student on 2025/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ refrigerator_cook/refrigerator_cookApp.swift
@@ -0,0 +1,17 @@
+//
+//  refrigerator_cookApp.swift
+//  refrigerator_cook
+//
+//  Created by dsu_student on 2025/11/18.
+//
+
+import SwiftUI
+
+@main
+struct refrigerator_cookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
